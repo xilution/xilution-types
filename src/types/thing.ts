5b5c1ff88e13593b5fa99c319f2568f6d103b2be
Todd Brunia <tbrunia@gmail.com>
2020-06-30 21:03:37 -0500

    fix: fixed links

--- src/types/thing.ts
+++ src/types/thing.ts
@@ -1,10 +1,11 @@
 import {IAPILink} from "./api";
+import {ILink} from "./link";
 
 export interface IThing {
   "@type"?: string;
   id?: string;
   owningUserId?: string;
-  links?: IAPILink[];
+  links?: ILink[] | IAPILink[];
   createdAt?: string;
   modifiedAt?: string;
   [key: string]: any;
