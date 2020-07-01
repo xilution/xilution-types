5a0aaf7781cb7f65206a5c68e760e6b4f164e627
Todd Brunia <tbrunia@gmail.com>
2020-06-30 20:41:57 -0500

    feat: added props to thing and organization

--- src/types/organization.ts
+++ src/types/organization.ts
@@ -3,7 +3,9 @@ import { IThing } from "./thing";
 export interface IOrganization extends IThing {
   id: string;
   name: string;
+  active?: boolean;
   logo?: string;
   domain?: string;
   iamClientId?: string;
+  organizationId?: string;
 }
--- src/types/thing.ts
+++ src/types/thing.ts
@@ -1,5 +1,9 @@
+import {IAPILink} from "./api";
+
 export interface IThing {
   "@type"?: string;
   id?: string;
+  owningUserId?: string;
+  links?: IAPILink[];
   [key: string]: any;
 }
