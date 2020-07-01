715c6fa23264ec3ac13ca44c039e26b289981b4e
Todd Brunia <tbrunia@gmail.com>
2020-06-30 21:13:44 -0500

    fix: updated cc

--- src/types/credit-card.ts
+++ src/types/credit-card.ts
@@ -1,8 +1,9 @@
 export interface ICreditCard {
   id?: string;
-  cvc: string;
+  cvc?: string;
   expMonth: number;
   expYear: number;
   name: string | null;
-  number: string;
+  number?: string;
+  brand: 'Visa' | 'American Express' | 'MasterCard' | 'Discover' | 'JCB' | 'Diners Club' | 'Unknown';
 }
