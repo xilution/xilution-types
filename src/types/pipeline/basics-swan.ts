c0e3e9ebb0fad6a4b2a8011d2d01accd520c22ca
Todd Brunia <tbrunia@gmail.com>
2021-03-17 12:42:47 -0500

    feat: removed repo property from swan

--- src/types/pipeline/basics-swan.ts
+++ src/types/pipeline/basics-swan.ts
@@ -3,7 +3,6 @@ import { IGit, IPipeline, IPipelineDetails } from "./shared";
 export interface ISwanPipeline extends IPipeline {
   provider: string;
   accountId: string;
-  repo: string;
 }
 
 export interface ISwanPipelineDetails extends IPipelineDetails {
