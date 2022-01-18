2c36a594771477a6938ac7fe7e36083b9926c171
Todd Brunia <tbrunia@gmail.com>
2022-01-18 08:34:54 -0600

    chore: added more capability types

--- src/types/reservable-asset.ts
+++ src/types/reservable-asset.ts
@@ -5,6 +5,9 @@ import { IAdditionalProperties } from "./shared";
 export enum CapabilityTypes {
   EQUALS = "equal",
   LESS_THAN_OR_EQUAL_TO = "less_than_or_equal_to",
+  GREATER_THAN_OR_EQUAL_TO = "greater_than_or_equal_to",
+  LESS_THAN = "less_than",
+  GREATER_THAN = "greater_than",
 }
 
 export interface ICapability {
